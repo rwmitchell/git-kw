df787f2deada5b7a207eaa4fed1721feb832b862
Richard Mitchell <rwmitchell@gmail.com>
2022-03-10 00:40:53 +0000

    Fix escaping backslashes - gitkwshrink

--- Source/gitkwshrink.sh
+++ Source/gitkwshrink.sh
@@ -13,12 +13,12 @@ ID=$( git show -s --date=format:"%F %T %z" --format="$( basename $PWD) %ad %d %h
 if [[ $FN ]]; then
   FQ=$PWD/$FN
 
-  sed "s#\$MyId.*\$#\$MyId\$#; \
-       s#\$Source.*\$#\$Source\$#; \
-       s#\$Date.*\$#\$Date\$#"     \
+  sed "s#\\\$MyId.*\\\$#\\\$MyId\\\$#; \
+       s#\\\$Source.*\\\$#\\\$Source\\\$#; \
+       s#\\\$Date.*\\\$#\\\$Date\\\$#"     \
        < $FN
 else
-  sed "s#\$MyId.*\$#\$MyId\$#; \
-       s#\$Source.*\$#\$Source\$#; \
-       s#\$Date.*\$#\$Date\$#"
+  sed "s#\\\$MyId.*\\\$#\\\$MyId\\\$#; \
+       s#\\\$Source.*\\\$#\\\$Source\\\$#; \
+       s#\\\$Date.*\\\$#\\\$Date\\\$#"
 fi
