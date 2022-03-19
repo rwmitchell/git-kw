24e15ae5e45d6a7757fc5c776cb3f410304e35a5
Richard Mitchell <rwmitchell@gmail.com>
2022-03-19 13:59:50 -0400

    Disable Log keyword expansion
    • Log expansion is breaking on nur_util.c for non-obvious reasons (keyword is not used)

--- Source/gitkwexpand.sh
+++ Source/gitkwexpand.sh
@@ -5,23 +5,30 @@
 # when invoked from git, there are no cmdline args
 
 # FN=$1
+
+
 FN=$(printf "%s" "$@" | sed -e 's,/,\\/,g' -e 's,&,\\&,g')
 FQ=$( git rev-parse --show-toplevel )
 DT=$( git show -s --date=format:"%F %T %z" --format="%ad" )
 ID=$( git show -s --date=format:"%F %T %z" --format="$( basename $PWD) %ad %d %h" )
 AN=$( git show -s --format="%an <%aE>" )
-LG=$( git log -n 3 --format="%aD%n%B" -- $FN )
-LG=$( echo "$LG" | sed 's/^/  /' )
-LG=${LG//$'\n'/\\n}   # escape newlines for sed
+
+# 2022-03-19: sed breaks on nur_util.c, disabling
+# LG=$( git log -n 3 --format="%aD%n%B" -- $FN )
+# LG=$( echo "$LG" | sed 's/^/  /;s/^ *$//' )   # insert two space before each line
+# LG=${LG//$'\n'/\\n}                          # escape newlines for sed
 
   printf "Updating keywords: %s|%s\n" "$0" "$*" > /dev/tty
 
   sed "s#\\\$MyId.*\\\$#\\\$MyId: $ID \\\$#;     \
        s#\\\$Source.*\\\$#\\\$Source: $FQ \\\$#; \
        s#\\\$Date.*\\\$#\\\$Date: $DT \\\$#;     \
-       s#\\\$Auth.*\\\$#\\\$Auth: $AN \\\$#;
-       s#\\\$File.*\\\$#\\\$File: $FN \\\$#;     \
-       s#\\\$Log.*\\\$#\\\$Log:\\n$LG \n  :Log\\\$#"     \
+       s#\\\$Auth.*\\\$#\\\$Auth: $AN \\\$#;     \
+       s#\\\$File.*\\\$#\\\$File: $FN \\\$#"
+#      s#\\\$Log.*\\\$#\\\$Log:\\n$LG\n  :Log\\\$#"
+
+# printf "FN :  %s\n" "$FN" > /dev/tty
+# printf "LOG:\n%s\n" "$LG" > /dev/tty
 
 exit 0;
 
