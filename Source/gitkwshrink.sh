37527d987d9061bda70708c76c3994bce8f44901
Richard Mitchell <rwmitchell@gmail.com>
2022-03-19 20:07:13 -0400

    Remove Log keyword
    • original cvs ‘Log’ used in ‘snapr’
    • my version used a closing keyword to shrink multiple lines
    • without closing keyword, file gets truncated on ‘git commit’

--- Source/gitkwshrink.sh
+++ Source/gitkwshrink.sh
@@ -12,15 +12,15 @@ if [[ $FN ]]; then
        s#\\\$Source.*\\\$#\\\$Source\\\$#; \
        s#\\\$Date.*\\\$#\\\$Date\\\$#;     \
        s#\\\$Auth.*\\\$#\\\$Auth\\\$#;     \
-       s#\\\$File.*\\\$#\\\$File\\\$#;     \
-       s#\\\$Log.*\\\$#\\\$Log\\\$#"       \
+       s#\\\$File.*\\\$#\\\$File\\\$#"
        < $FN
+#      s#\\\$Log.*\\\$#\\\$Log\\\$#"       \
 else
   sed "s#\\\$MyId.*\\\$#\\\$MyId\\\$#;     \
        s#\\\$Source.*\\\$#\\\$Source\\\$#; \
        s#\\\$Date.*\\\$#\\\$Date\\\$#;     \
        s#\\\$Auth.*\\\$#\\\$Auth\\\$#;     \
-       s#\\\$File.*\\\$#\\\$File\\\$#"     \
-  | sed -e '/$Log:/,/:Log\$/c\
-  \$Log\$'
+       s#\\\$File.*\\\$#\\\$File\\\$#"
+# | sed -e '/$Log:/,/:Log\$/c\
+# \$Log\$'
 fi
