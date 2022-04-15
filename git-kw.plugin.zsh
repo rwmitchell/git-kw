ab81658ea24df69fbfaf789cd32ab3f1058f3ceb
Richard Mitchell <rwmitchell@gmail.com>
2022-04-15 08:36:06 -0400

    Add switch/branch stuff from OMZ git plugin
    • gsw: git switch
    • gswc: git switch -c  # creates branch
    • grename(): rename branch - don’t like name

--- git-kw.plugin.zsh
+++ git-kw.plugin.zsh
@@ -256,3 +256,23 @@ function gmr() {
     yline
   done
 }
+
+# Copied from OMZ git plugin
+
+alias gsw='git switch'
+alias gswc='git switch -c'
+
+function grename() {
+  if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: $0 old_branch new_branch"
+    return 1
+  fi
+
+  # Rename branch locally
+  git branch -m "$1" "$2"
+  # Rename branch in origin remote
+  if git push origin :"$1"; then
+    git push --set-upstream origin "$2"
+  fi
+}
+
