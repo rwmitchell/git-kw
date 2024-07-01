939be27ab5f421fe83cc981c99abf12295e387d9
Richard Mitchell <rwmitchell@gmail.com>
2024-07-01 13:51:48 -0400

    gdwd(): add -a to commit all files together
    • prompts to show diffs for each file
    • prompts to commit after all files are shown

--- git-kw.plugin.zsh
+++ git-kw.plugin.zsh
@@ -1081,28 +1081,47 @@ function gdwd() {     # Show git diff using dwdiff
   is_git || return
 
   local prmpt rsp cmmit help
-  zparseopts -D -F -K p=prmpt c=cmmit h=help
+  zparseopts -D -F -K p=prmpt a=all c=cmmit h=help
 
   [[ $help ]] && {
     printf "Usage: %s [ -cph ]\n" "$0"
     printf "\nUse dwdiff to compare changes to repo\n"
+    printf "\t-a : commit all files together\n"
     printf "\t-c : prompt to commit     for each file\n"
     printf "\t-p : prompt to show diffs for each file\n"
     return 0
   }
 
-  for file in $( git ls-files --modified $@ )
-  do
-#   ( printf ">>>\e[1m\e[38;5;6m %s \e[0m<<<\n\n" $file; git difftool -y --tool=dwdiff $file ) # | $PAGER
-    [[ $prmpt ]] && rsp=$(prompt -e "show $file ?" "yY" "nN" "qa")
-    [[ "qa" == *$rsp* && $prmpt ]] && return 0
-    [[ "Yy" == *$rsp* || ! $prmpt ]] && \
-    ( printf ">>>\e[1m\e[38;5;6m %s \e[0m<<<\n\n" $file; git diff $file | dwdiff -u) # | $PAGER
-    lbline 2
-    [[ $cmmit ]] && rsp=$(prompt -e "Commit $file ?" "yY" "nN" "qa" )
-    [[ "qa" == *$rsp* && $cmmit ]] && return 0
-    [[ "Yy" == *$rsp* && $cmmit ]] && { printf "Committing $file\n"; gc $file }
-  done # | mdless     # mdless parses comments, ie '#',  as header lines
+  files=($( git ls-files --modified $@ ))
+
+  [[ $all ]] && {
+    for file in $files; do
+      rsp=$(prompt -e "show $file ?" "yY" "nN" "qa")
+      [[ "qa" == *$rsp* ]] && return 0
+      [[ "Yy" == *$rsp* ]] && \
+        ( printf ">>>\e[1m\e[38;5;6m %s \e[0m<<<\n\n" $file; git diff $file | dwdiff -u)
+    done
+    rsp=$(prompt -e "Commit $files ?" "yY" "nN" "qa" )
+    [[ "Yy" == *$rsp* ]] && { printf "Committing $file\n"; gc $files }
+
+  }
+
+  [[ ! $all ]] && {
+    for file in $files; do
+  #   ( printf ">>>\e[1m\e[38;5;6m %s \e[0m<<<\n\n" $file; git difftool -y --tool=dwdiff $file ) # | $PAGER
+      [[ $prmpt ]] && rsp=$(prompt -e "show $file ?" "yY" "nN" "qa")
+      [[ "qa" == *$rsp* && $prmpt ]] && return 0
+      [[ "Yy" == *$rsp* || ! $prmpt ]] && \
+      ( printf ">>>\e[1m\e[38;5;6m %s \e[0m<<<\n\n" $file; git diff $file | dwdiff -u) # | $PAGER
+      lbline 2
+      [[ $cmmit ]] && rsp=$(prompt -e "Commit $file ?" "yY" "nN" "qa" )
+      [[ "qa" == *$rsp* && $cmmit ]] && return 0
+      [[ "Yy" == *$rsp* && $cmmit ]] && { printf "Committing $file\n"; gc $file }
+    done # | mdless     # mdless parses comments, ie '#',  as header lines
+
+    [[ "Yy" == *$rsp* ]] && return 0 || return 1
+
+  }
 
 }
 compdef _git gdwd=git-diff
