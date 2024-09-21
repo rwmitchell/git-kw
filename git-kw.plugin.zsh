7c00ece08267b0bdb86f185c9fd9922afe162846
Richard Mitchell <rwmitchell@gmail.com>
2024-09-21 09:16:55 -0400

    gp: git push - testing hl fix - still not working

--- git-kw.plugin.zsh
+++ git-kw.plugin.zsh
@@ -205,9 +205,7 @@ function gp() {
           ssay "Updating $cnt files on $h!" | hl "^.*$"
           # Checking OLD/NEW here shows if there is an actual transfer
 #         OLD_COMMIT=$( git rev-parse $h/$branch )
-          # hl is removing everything not cyan, why??  Swapping order
-#         git push $mytags $h HEAD 2>&1 | hl -n -G "^Enum.*$|^Count.*$|^Delta.*$|^Comp.*$|^Writ.*$" -z -c "^To.*$"
-          git push $mytags $h HEAD 2>&1 | hl -n -G "^Enum.*$|^Count.*$|^Delta.*$|^Comp.*$|^Writ.*$"
+          git push $mytags $h HEAD |& hl -n -G "^Enum.*$|^Count.*$|^Delta.*$|^Comp.*$|^Writ.*$" -z -c "^To.*$"
           th=$h
 #         NEW_COMMIT=$( git rev-parse $h/$branch )
           [[ $? == 0 ]] && ((rc+=1))
